41899b57bfc7d95cbd9580fc18cd5d0e556ab51c
graysonmcmurry <graysonmcmurry@graysons-iMac.attlocal.net>
2020-10-09 00:40:41 -0500

    making trivia game work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,11 @@
 //array
 var words = ["lowtide", "clam", "coral", "boat", "coast", "hermit", "fish"];
 //random string from array
-let randomNum = Math.floor(Math.random() * words.length);
+const randomNum = () => {
+  return Math.floor(Math.random() * words.length);
+};
 //consolidated to variable
-let chosenWord = words[randomNum];
+let chosenWord = words[randomNum()];
 //open array for all the right letters chosen
 let rightWordArray = [];
 // open array for all the letters guessed/wrong
@@ -16,18 +18,22 @@ let wins = 0;
 let winsCount = document.getElementsByClassName("winscount");
 let guessremainingNum = 15;
 let guessremainingNumSpan = document.getElementById("guessremainingNum");
+let imgDiv = document.getElementById("imgdivid");
 
-console.log(chosenWord);
 //not working properly unable to reload image and create a new word rather than existing
 function resetFunc() {
+  chosenWord = words[randomNum()];
+  imgDiv.removeChild(imgDiv.childNodes[2]);
+  newPicture();
   guessremainingNum = 15;
   guessremainingNumSpan.innerHTML = guessremainingNum;
   rightWordArray = [];
   wrongWordArray = [];
   underScore = [];
   docUnderScore[0].innerHTML = generateUnderscore().join(" ");
-  generateUnderscore;
 }
+
+function randomTrivia() {}
 
 function generateUnderscore() {
   for (let i = 0; i < chosenWord.length; i++) {
@@ -35,42 +41,36 @@ function generateUnderscore() {
   }
   return underScore;
 }
-
-if (chosenWord === words[0]) {
-  var img = document.createElement("img");
-  var src = document.getElementsByClassName("imgdivclass");
-  img.src = "assets/images/lowtide.jpg";
-  document.getElementById("imgdivid").appendChild(img);
-} else if (chosenWord === words[1]) {
-  var img = document.createElement("img");
-  var src = document.getElementsByClassName("imgdivclass");
-  img.src = "assets/images/clam.jpg";
-  document.getElementById("imgdivid").appendChild(img);
-} else if (chosenWord === words[2]) {
-  var img = document.createElement("img");
-  var src = document.getElementsByClassName("imgdivclass");
-  img.src = "assets/images/coral.jpg";
-  document.getElementById("imgdivid").appendChild(img);
-} else if (chosenWord === words[3]) {
-  var img = document.createElement("img");
-  var src = document.getElementsByClassName("imgdivclass");
-  img.src = "assets/images/boat.jpg";
-  document.getElementById("imgdivid").appendChild(img);
-} else if (chosenWord === words[4]) {
-  var img = document.createElement("img");
-  var src = document.getElementsByClassName("imgdivclass");
-  img.src = "assets/images/coast.jpg";
-  document.getElementById("imgdivid").appendChild(img);
-} else if (chosenWord === words[5]) {
-  var img = document.createElement("img");
-  var src = document.getElementsByClassName("imgdivclass");
-  img.src = "assets/images/hermit.jpg";
-  document.getElementById("imgdivid").appendChild(img);
-} else if (chosenWord === words[6]) {
-  var img = document.createElement("img");
-  var src = document.getElementsByClassName("imgdivclass");
-  img.src = "assets/images/fish.jpg";
-  document.getElementById("imgdivid").appendChild(img);
+function newPicture() {
+  if (chosenWord === words[0]) {
+    var img = document.createElement("img");
+    img.src = "assets/images/lowtide.jpg";
+    imgDiv.appendChild(img);
+  } else if (chosenWord === words[1]) {
+    var img = document.createElement("img");
+    img.src = "assets/images/clam.jpg";
+    imgDiv.appendChild(img);
+  } else if (chosenWord === words[2]) {
+    var img = document.createElement("img");
+    img.src = "assets/images/coral.jpg";
+    imgDiv.appendChild(img);
+  } else if (chosenWord === words[3]) {
+    var img = document.createElement("img");
+    img.src = "assets/images/boat.jpg";
+    imgDiv.appendChild(img);
+  } else if (chosenWord === words[4]) {
+    var img = document.createElement("img");
+    img.src = "assets/images/coast.jpg";
+    imgDiv.appendChild(img);
+  } else if (chosenWord === words[5]) {
+    var img = document.createElement("img");
+    img.src = "assets/images/hermit.jpg";
+    imgDiv.appendChild(img);
+  } else if (chosenWord === words[6]) {
+    var img = document.createElement("img");
+    img.src = "assets/images/fish.jpg";
+    imgDiv.appendChild(img);
+  }
 }
 
 // creating the function to minus the number of guesses when a letter is pushed to wrongWordArray
@@ -84,7 +84,7 @@ function wrongGuess() {
   }
 }
 
-document.addEventListener("keyup", function() {
+document.addEventListener("keyup", function () {
   let keyword = String.fromCharCode(event.keyCode).toLowerCase();
   if (chosenWord.indexOf(keyword) > -1) {
     rightWordArray.push(keyword);
@@ -104,5 +104,4 @@ document.addEventListener("keyup", function() {
   docGuess[0].innerHTML = wrongWordArray.join(" ");
 });
 
-// creates underscores in the DOM
-docUnderScore[0].innerHTML = generateUnderscore().join(" ");
+resetFunc();
